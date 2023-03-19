77dfd181ee076e4ea8efc882da8c51042788c632
João Victor da Silva Coutinho <joao.coutinho@compasso.com.br>
2023-03-19 14:00:04 -0300

    Add: check if getWinner has been called twice and if the values are correct

--- src/__tests__/mock-fn.js
+++ src/__tests__/mock-fn.js
@@ -14,20 +14,36 @@
  * Execute: Use `npx jest --watch src/__tests__/mock-fn.js` to watch the test
  */
 
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const thumbWar = require("../thumb-war");
+const utils = require("../utils");
 
-test('returns winner', () => {
-  const originalGetWinner = utils.getWinner
-  utils.getWinner = jest.fn((p1, p2) => p1)
+test("returns winner", () => {
+	const originalGetWinner = utils.getWinner;
+	utils.getWinner = jest.fn((p1, p2) => p1);
 
-  const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-  expect(winner).toBe('Kent C. Dodds')
-  // Your code:
+	const winner = thumbWar("Kent C. Dodds", "Ken Wheeler");
+	expect(winner).toBe("Kent C. Dodds");
+	// Your code:
+	expect(utils.getWinner.mock.calls).toEqual([
+		["Kent C. Dodds", "Ken Wheeler"],
+		["Kent C. Dodds", "Ken Wheeler"],
+	]);
+	// expect(utils.getWinner).toHaveBeenCalledTimes(2); // usa 2x dentro do thumbWar
+	// expect(utils.getWinner).toHaveBeenCalledWith("Kent C. Dodds", "Ken Wheeler");
+	// expect(utils.getWinner).toHaveBeenNthCalledWith(
+	// 	1,
+	// 	"Kent C. Dodds",
+	// 	"Ken Wheeler"
+	// );
+	// expect(utils.getWinner).toHaveBeenNthCalledWith(
+	// 	2,
+	// 	"Kent C. Dodds",
+	// 	"Ken Wheeler"
+	// );
 
-  // cleanup
-  utils.getWinner = originalGetWinner
-})
+	// cleanup
+	utils.getWinner = originalGetWinner;
+});
 
 /**
  * Hints:
