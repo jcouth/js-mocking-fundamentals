4fe7c18b47c851147f0ebe5e54fe6087b01579a3
João Victor da Silva Coutinho <joao.coutinho@compasso.com.br>
2023-03-19 19:26:12 -0300

    Add: mockUtilsPath to use mocks from __no-framework-mocks__

--- src/no-framework/external-mock-module.js
+++ src/no-framework/external-mock-module.js
@@ -4,38 +4,24 @@
  * Execute: Use `npx jest --watch src/no-framework/external-mock-module.js` to watch the test
  */
 
-function fn(impl = () => {}) {
-  const mockFn = (...args) => {
-    mockFn.mock.calls.push(args)
-    return impl(...args)
-  }
-  mockFn.mock = {calls: []}
-  return mockFn
-}
+require("../__no-framework-mocks__/utils");
+const utilsPath = require.resolve("../utils");
+const mockUtilsPath = require.resolve("../__no-framework-mocks__/utils");
+require.cache[utilsPath] = require.cache[mockUtilsPath];
 
-const utilsPath = require.resolve('../utils')
-require.cache[utilsPath] = {
-  id: utilsPath,
-  filename: utilsPath,
-  loaded: true,
-  exports: {
-    getWinner: fn((p1, p2) => p1),
-  },
-}
+const assert = require("assert");
+const thumbWar = require("../thumb-war");
+const utils = require("../utils");
 
-const assert = require('assert')
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
-
-const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-assert.strictEqual(winner, 'Kent C. Dodds')
+const winner = thumbWar("Kent C. Dodds", "Ken Wheeler");
+assert.strictEqual(winner, "Kent C. Dodds");
 assert.deepStrictEqual(utils.getWinner.mock.calls, [
-  ['Kent C. Dodds', 'Ken Wheeler'],
-  ['Kent C. Dodds', 'Ken Wheeler'],
-])
+	["Kent C. Dodds", "Ken Wheeler"],
+	["Kent C. Dodds", "Ken Wheeler"],
+]);
 
 // cleanup
-delete require.cache[utilsPath]
+delete require.cache[utilsPath];
 
 /**
  * Checkout master branch to see the answer.
