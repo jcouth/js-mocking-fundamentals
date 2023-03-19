9834e220ded4542a723829b95fd86e585a471a2a
João Victor da Silva Coutinho <joao.coutinho@compasso.com.br>
2023-03-19 19:06:11 -0300

    Add: mockImplementation and mockRestore

--- src/__tests__/spy.js
+++ src/__tests__/spy.js
@@ -11,23 +11,23 @@
  * Execute: Use `npx jest --watch src/__tests__/spy.js` to watch the test
  */
 
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const thumbWar = require("../thumb-war");
+const utils = require("../utils");
 
-test('returns winner', () => {
-  const originalGetWinner = utils.getWinner
-  utils.getWinner = jest.fn((p1, p2) => p1)
+test("returns winner", () => {
+	jest.spyOn(utils, "getWinner");
+	utils.getWinner.mockImplementation((p1, p2) => p1);
 
-  const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-  expect(winner).toBe('Kent C. Dodds')
-  expect(utils.getWinner.mock.calls).toEqual([
-    ['Kent C. Dodds', 'Ken Wheeler'],
-    ['Kent C. Dodds', 'Ken Wheeler'],
-  ])
+	const winner = thumbWar("Kent C. Dodds", "Ken Wheeler");
+	expect(winner).toBe("Kent C. Dodds");
+	expect(utils.getWinner.mock.calls).toEqual([
+		["Kent C. Dodds", "Ken Wheeler"],
+		["Kent C. Dodds", "Ken Wheeler"],
+	]);
 
-  // cleanup
-  utils.getWinner = originalGetWinner
-})
+	// cleanup
+	utils.getWinner.mockRestore();
+});
 
 /**
  * Hints:
