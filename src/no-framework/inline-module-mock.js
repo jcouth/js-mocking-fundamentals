cf7bb01730ffbc9c82925edaa2bc5796a79cf367
João Victor da Silva Coutinho <joao.coutinho@compasso.com.br>
2023-03-19 19:19:56 -0300

    Add: getWinner to require cache

--- src/no-framework/inline-module-mock.js
+++ src/no-framework/inline-module-mock.js
@@ -5,26 +5,37 @@
  */
 
 function fn(impl = () => {}) {
-  const mockFn = (...args) => {
-    mockFn.mock.calls.push(args)
-    return impl(...args)
-  }
-  mockFn.mock = {calls: []}
-  return mockFn
+	const mockFn = (...args) => {
+		mockFn.mock.calls.push(args);
+		return impl(...args);
+	};
+	mockFn.mock = { calls: [] };
+	return mockFn;
 }
 
-const assert = require('assert')
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const utilsPath = require.resolve("../utils");
+require.cache[utilsPath] = {
+	id: utilsPath,
+	filename: utilsPath,
+	loaded: true,
+	exports: {
+		getWinner: fn((p1, p2) => p1),
+	},
+};
 
-const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-assert.strictEqual(winner, 'Kent C. Dodds')
+const assert = require("assert");
+const thumbWar = require("../thumb-war");
+const utils = require("../utils");
+
+const winner = thumbWar("Kent C. Dodds", "Ken Wheeler");
+assert.strictEqual(winner, "Kent C. Dodds");
 assert.deepStrictEqual(utils.getWinner.mock.calls, [
-  ['Kent C. Dodds', 'Ken Wheeler'],
-  ['Kent C. Dodds', 'Ken Wheeler'],
-])
+	["Kent C. Dodds", "Ken Wheeler"],
+	["Kent C. Dodds", "Ken Wheeler"],
+]);
 
 // cleanup
+delete require.cache[utilsPath];
 
 /**
  * Hints:
